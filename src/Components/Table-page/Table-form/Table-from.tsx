c983f8594d0950ef03ec43eb1bfc10890e787533
Juthaporn Simmalee <idea_par@hotmail.com>
2019-11-07 14:07:42 +0700

    update table
    update table

--- src/Components/Table-page/Table-form/Table-from.tsx
+++ src/Components/Table-page/Table-form/Table-from.tsx
@@ -3,15 +3,69 @@ import "./Table-form.scss"
 const TableForm = () => {
     return (
         <div className="marpage">
-            <label className="text">ปี 1</label>
+            <div>
+                <label className="text">ปี 1</label>
+                <table>
+                    <caption >ปี 1 เทอม 1</caption>
+                    {/*head row*/}
+                    <tr>
+                        <th>รหัสวิชา</th>
+                        <th>ชื่อวิชา</th>
+                        <th>หน่วยกิต</th>
+                        <th>เกรด</th>
+                    </tr>
+                    {/*row 1*/}
+                    <tr>
+                        <td>202113</td>
+                        <td>Physics I</td>
+                        <td>3</td>
+                        <td>D</td>
+                    </tr>
+                    {/*row 2*/}
+                    <tr>
+                        <td>202113</td>
+                        <td>Physics I</td>
+                        <td>3</td>
+                        <td>D</td>
+                    </tr>
+                    {/*row 3*/}
+                    <tr>
+                        <td>202113</td>
+                        <td>Physics I</td>
+                        <td>3</td>
+                        <td>D</td>
+                    </tr>
+                    {/*row 4*/}
+                    <tr>
+                        <td>202113</td>
+                        <td>Physics I</td>
+                        <td>3</td>
+                        <td>D</td>
+                    </tr>
+                    {/*row 5*/}
+                    <tr>
+                        <td>202113</td>
+                        <td>Physics I</td>
+                        <td>3</td>
+                        <td>D</td>
+                    </tr>
+                    {/*row 6*/}
+                    <tr>
+                        <td>202113</td>
+                        <td>Physics I</td>
+                        <td>3</td>
+                        <td>D</td>
+                    </tr>
+                </table>
+            </div>
+
             {/* <label>ปี 2</label>
             <label>ปี 3</label>
             <label>ปี 4</label> */}
 
 
 
-        </div>
-
+        </div >
     );
 };
 export default TableForm;
