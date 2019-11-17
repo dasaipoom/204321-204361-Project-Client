a06210dd2e574ad90582fd214f685ee01b7dc8a7
A-Oi <poomchaiprasop@gmail.com>
2019-11-17 20:36:42 +0700

    Yes i got my table

--- src/Components/Table-page/Table-ele.tsx
+++ src/Components/Table-page/Table-ele.tsx
@@ -1,7 +1,38 @@
 import React from "react";
+import { render } from "react-dom";
 
-function TableEle() {
-  return <></>;
+function TableEle(element) {
+  const { CourseID, CourseName, CourseCredit, Grade } = element.element;
+  if (Grade == "P") {
+    return (
+      
+      <tr>
+        <td>{CourseID}</td>
+        <td>{CourseName}</td>
+        <td>{CourseCredit}</td>
+        <td>
+          <input 
+        
+          className="input is-small"
+          type="EditedGrade"
+          />
+        </td>
+      </tr>
+     
+    );
+  }  
+  else{
+    return (
+      
+      <tr>
+        <td>{CourseID}</td>
+        <td>{CourseName}</td>
+        <td>{CourseCredit}</td>
+        <td>{Grade}</td>
+      </tr>
+     
+    );
+  }
 }
 
 export default TableEle;
